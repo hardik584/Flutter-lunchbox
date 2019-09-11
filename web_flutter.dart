7a5e6d17422926bf0b3835f76af2c96959f745d0
Hardik Kumbhani <phardik584@gmail.com>
2019-09-11 17:45:45 +0530

    Create web_flutter.dart

---
+++ web_flutter.dart
@@ -0,0 +1,8 @@
+flutter channel master
+flutter upgrade
+flutter config --enable-web
+cd <into project directory>
+flutter create .
+flutter run -d chrome
+
+https://flutter.dev/docs/get-started/web
