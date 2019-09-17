322c729cc567074466eebecee17fda413d0140a3
Hardik Kumbhani <phardik584@gmail.com>
2019-09-17 14:44:28 +0530

    Update Firebase and http.dart

--- Firebase and http.dart
+++ Firebase and http.dart
@@ -1,5 +1,7 @@
    
    //for http
    await http.get("https://reqres.in/api/users?page=1").then((onValue) {});
-   //
    
+   //for Firebase
+  final messageReference =
+      FirebaseDatabase.instance.reference().child('message_master');
