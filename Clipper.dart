42a66eb03c09b015904bb0c5b9815f7f2634a9af
Hardik Kumbhani <phardik584@gmail.com>
2019-09-23 15:23:07 +0530

    Update Clipper.dart

--- Clipper.dart
+++ Clipper.dart
@@ -28,7 +28,53 @@ class CustomShapeClipper extends CustomClipper<Path> {
   bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 } 
 ************************************************************************************************************************
+                                    *
+                                *
+                            *
+                       *
+                  *
+            *      
+       *
+ *
+ import 'package:flutter/material.dart';
+
+class CustomShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0.0, size.height - 110);
+    var secondEndpoint = Offset(size.width, size.height - 30.0);
+    var secondControlPoint = Offset(size.width * .50, size.height - 30);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndpoint.dx, secondEndpoint.dy);
+    path.lineTo(size.width, 0.0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
+
+class CustomShapeClipper2 extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0.0, size.height - 105);
+    var secondEndpoint = Offset(size.width, size.height - 40.0);
+    var secondControlPoint = Offset(size.width * .50, size.height - 30);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndpoint.dx, secondEndpoint.dy);
+    path.lineTo(size.width, 0.0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
  
-  
   
  
