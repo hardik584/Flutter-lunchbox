be94839a7450df36a9bf4d7cf1b748bbce3d1cd5
Aespatel97 <55019178+Aespatel97@users.noreply.github.com>
2019-09-24 11:44:45 +0530

    Update animatedIcon.dart

--- animatedIcon.dart
+++ animatedIcon.dart
@@ -1,1 +1,46 @@
 https://www.youtube.com/watch?v=sb92CI312oQ
+
+
+import 'package:flutter/material.dart';
+ 
+
+class WheelExample extends StatefulWidget {
+  @override
+  _WheelExampleState createState() => _WheelExampleState();
+}
+
+class _WheelExampleState extends State<WheelExample> with SingleTickerProviderStateMixin{
+  int a;
+  AnimationController anime;
+  bool isplaying = true;
+
+  @override
+  void initState() {
+    super.initState();
+   anime = AnimationController(duration: Duration(milliseconds: 600),vsync: this);
+
+  }
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wheel'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: (){
+            anime.forward();
+          },
+          onDoubleTap: (){
+            anime.reverse();
+          },
+                  child: Container(
+            child: AnimatedIcon(icon: AnimatedIcons.arrow_menu,progress: anime,size: 45,),
+          ),
+        ),
+      ),
+    );
+  }
+}
