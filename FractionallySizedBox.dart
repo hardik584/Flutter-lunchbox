14f26ce292ea087e909749ac7fb43974c5319b54
Hardik Kumbhani <phardik584@gmail.com>
2019-09-30 10:40:26 +0530

    Create FractionallySizedBox.dart

---
+++ FractionallySizedBox.dart
@@ -0,0 +1,37 @@
+ Row(
+                      children: <Widget>[
+                        Flexible(
+                          fit: FlexFit.tight,
+                          flex: 5,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.9,
+                            child: RaisedButton(
+                              child: Text("data"),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          flex: 5,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.9,
+                            child: RaisedButton(
+                              child: Text("data"),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          flex: 5,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.9,
+                            child: RaisedButton(
+                              child: Text("data"),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
