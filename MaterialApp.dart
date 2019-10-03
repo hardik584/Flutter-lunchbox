0c8363a0765ad6f2625868d33a4d2156046efc56
Hardik Kumbhani <phardik584@gmail.com>
2019-10-03 17:10:53 +0530

    Create MaterialApp.dart

---
+++ MaterialApp.dart
@@ -0,0 +1,9 @@
+MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+  );
+  
+ when you want to utilize this color somewhere else in your app then you can use like
+ just instance:
+ color:Theme.of(context).primaryColor
