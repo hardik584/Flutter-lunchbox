c74386e930449e61d7c832f5aff5f049469b2b19
Hardik Kumbhani <phardik584@gmail.com>
2019-10-04 15:40:19 +0530

    Update blueprint.dart

--- blueprint.dart
+++ blueprint.dart
@@ -1,4 +1,15 @@
   import 'package:flutter/rendering.dart';
   void main()  {
+  debugPaintBaselinesEnabled = true;
+  debugPaintLayerBordersEnabled = true;
+  debugRepaintRainbowEnabled = true;
   debugPaintSizeEnabled = true;
+  debugPaintPointersEnabled = true;
+  debugCheckElevationsEnabled = true;
+  debugCheckIntrinsicSizes = true;
+  debugDisableClipLayers = true;
+  debugDisableOpacityLayers = true;
+  debugDisablePhysicalShapeLayers = true;
+  debugDisableShadows = true;
+
   runApp(MyApp());}
