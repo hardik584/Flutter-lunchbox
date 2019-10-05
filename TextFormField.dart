012c0e095a66295c6a7b2aa4cc712c30c6e3e5dd
Hardik Kumbhani <phardik584@gmail.com>
2019-10-05 15:10:57 +0530

    Create TextFormField.dart

---
+++ TextFormField.dart
@@ -0,0 +1,25 @@
+ TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Align(
+                      alignment: Alignment(-1, 0),
+                      widthFactor: 0.0,
+                      child: Icon(
+                        Icons.security,
+                        color: Colors.white,
+                      ),
+                    ),
+                
+                    hintText: "Enter Password",
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 0.6),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: AppColor.primarySwatch, width: 0.6),
+                    ),
+                  ),
+                ),
+********************************************************************************************
