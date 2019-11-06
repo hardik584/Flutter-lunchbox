f0c5ed4e701614a24a8ea70d9b34e282cfa71794
Hardik Kumbhani <phardik584@gmail.com>
2019-11-06 12:20:39 +0530

    Create apple config.dart

---
+++ apple config.dart
@@ -0,0 +1,13 @@
+  
+1. First of all we have to install pod in ios folder for run in ios device
+  open ios folder from your project and open terminal
+  => enter this command in termial 
+ $ sudo gem install cocoapods
+ $ pod setup
+ //make sure that you have already specify version of ios in your general file.
+ 
+2. After that you have to refresh your xcode and build the app if there are something absence then you have to add into Podfile.lock
+Like example and repeat 1 step for get that package in pod
+  pod "BSImagePicker", "~> 2.8"
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Core'
