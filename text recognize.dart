9b619d27ad732cf5adc7fc0ce538a793955f5a66
Hardik Kumbhani <phardik584@gmail.com>
2019-12-02 12:32:58 +0530

    Create text recognize.dart

---
+++ text recognize.dart
@@ -0,0 +1,79 @@
+// add below code in pubspec.yaml file
+module:
+  androidX: true
+firebase_ml_vision: ^0.9.3+3
+image_picker: ^0.6.2+1
+
+
+
+import 'dart:io';
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class TextReco extends StatefulWidget {
+  TextReco({Key key}) : super(key: key);
+
+  @override
+  _TextRecoState createState() => _TextRecoState();
+}
+
+class _TextRecoState extends State<TextReco> {
+  File _image;
+
+  String text;
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+    readText().then((onValue) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(onValue),
+            );
+          });
+    });
+  }
+
+  Future<String> readText() async {
+    text = "";
+
+    FirebaseVisionImage ourimage = FirebaseVisionImage.fromFile(_image);
+    TextRecognizer reconizetext = FirebaseVision.instance.textRecognizer();
+    VisionText readtext = await reconizetext.processImage(ourimage);
+
+    for (TextBlock block in readtext.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement word in line.elements) {
+          // print(word.text);
+
+          text += word.text + " ";
+          print(text);
+        }
+      }
+    }
+    return text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text Reco"),
+      ),
+      body: Center(
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
