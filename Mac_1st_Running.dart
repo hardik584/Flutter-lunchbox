8f1616e3497ae7f2552e48c181c0f4b8ca537820
Aespatel97 <55019178+Aespatel97@users.noreply.github.com>
2019-12-04 15:48:00 +0530

    Create Mac_1st_Running.dart

---
+++ Mac_1st_Running.dart
@@ -0,0 +1,9 @@
+1. Go to build Setting
+ Flutter Application path
+ /Users/infi_mini/Desktop/Yoshan-master // Write your project name instead of Yoshan-master
+
+2. Flutter Root
+/Users/infi_mini/Desktop/Kartavya/Extra/flutter
+
+3. Flutter target
+/Users/infi_mini/Desktop/Yoshan-master/lib/main.dart
