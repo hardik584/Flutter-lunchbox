b55e1505b84d65bf98d89136197b1a0c7227b659
Hardik Kumbhani <phardik584@gmail.com>
2019-12-06 10:08:59 +0530

    Update Mac_1st_Running.dart

--- Mac_1st_Running.dart
+++ Mac_1st_Running.dart
@@ -10,3 +10,17 @@
 
 4 Flutter framework dir
  /Users/infi_mini/Desktop/Kartavya/Extra/flutter/bin/cache/artifacts/engine/ios
+
+ 
+ When you are transfer your project from somewhere 
+ 
+ then you should fire below command in root dir of that project 
+    #  flutter packages get
+ after that 
+    go to ios floder and open terminal
+    1. sudo gem install cocoapods
+    2. pod setup 
+    3. pod init
+    4. pod install
+   and when you have already pod file you want to update pod then fire below command
+    5. pod repo update
