1506ce9fed134b8001daba65b68c438cdb2aabc2
Hardik Kumbhani <phardik584@gmail.com>
2019-12-21 12:26:39 +0530

    Create handle_Red_error.dart

---
+++ handle_Red_error.dart
@@ -0,0 +1,16 @@
+
+//https://flutter.dev/docs/testing/errors
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+  runApp(MyApp());
+}
