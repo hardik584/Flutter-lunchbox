e4c87d031a9006b74c9b70bd7c2c5284d075dc2c
Aespatel97 <55019178+Aespatel97@users.noreply.github.com>
2019-12-21 10:20:32 +0530

    Update library.dart

--- library.dart
+++ library.dart
@@ -1,3 +1,6 @@
 dots_indicator  for swiper
 For Create Flutter project with package name and app name
 flutter create --org com.yourdomain vayro
+  
+/////Circle Range Slider
+https://pub.dev/packages/flutter_circular_slider
