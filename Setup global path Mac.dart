eac9c70d0783d7f5253a9bb7e802040045c3a2d4
Hardik Kumbhani <phardik584@gmail.com>
2020-01-06 12:00:47 +0530

    Create Setup global path Mac.dart

---
+++ Setup global path Mac.dart
@@ -0,0 +1,10 @@
+open terminal
+ =>1 nano ~/.bash_profile. 
+ //When you are fire this command then you will get one notepad like screen in terminal and then you have to follow below command 
+ =>2 paste export PATH=$PATH://Users/iconflux/Documents/Flutter\ SDK/flutter/bin 
+           inside it and press ctrl + x and save that file
+ =>3 after successfully doing above process
+     fire this command in existing window 
+     source $HOME/.bash_profile
+ =>4 now you can enjoy flutter anywhere in your mac os 
+ 
