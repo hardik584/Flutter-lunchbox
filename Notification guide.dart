80a395d9752f98a0d767072193cda5de0ca53cc3
Hardik Kumbhani <phardik584@gmail.com>
2020-01-09 15:22:54 +0530

    Create Notification guide.dart

---
+++ Notification guide.dart
@@ -0,0 +1,5 @@
+https://medium.com/@duytq94/flutter-chat-app-extended-push-notification-messages-a26c669f4675
+https://backendless.com/developers/#
+check out above link for push notification 
+There so many but like onesignal, IBM, Firebase, AWS push
+You can  choose any one from them.
