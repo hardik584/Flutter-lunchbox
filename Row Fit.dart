cdb6df3db38057652d3a9a291433d51372adfd77
Hardik Kumbhani <phardik584@gmail.com>
2020-02-04 18:02:35 +0530

    Create Row Fit.dart

---
+++ Row Fit.dart
@@ -0,0 +1,4 @@
+https://stackoverflow.com/questions/51326170/flutter-layout-row-column-share-width-expand-height
+   IntrinsicHeight(
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.stretch,
